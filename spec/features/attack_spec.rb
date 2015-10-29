0a28a4bf0fde163f91fd2c32eb87b07b48e54d9c
mateja683 <mateja683@gmail.com>
2015-10-29 12:54:39 +0000

    amending attack function on chapter 22

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 feature 'attack player and receive confirmation' do
   scenario 'attack players 2' do
     sign_in_and_play
-    expect(page).to have_content "You have attacked P2"
+    click_on 'Attack'
+    expect(page).to have_content "P1 attacked P2"
   end
 end
